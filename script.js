d7534e10ebbfd2db1456ee7a0a9c34982121995f
Aceer121 <45231128+Aceer121@users.noreply.github.com>
2022-04-13 20:55:59 -0400

    const count = '10'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Unsplash API
-const count = 10;
+const count = '10';
 const apiKey = 'GHrhY0_y7gGc8qW2wW9lJAKgcErDVcgWX42g0lYA4C8';
 const apiURL = 'https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${count}';
 
